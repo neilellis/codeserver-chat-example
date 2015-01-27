6c85d63fc31a91b0f6b0cc313d2c1a00829e9466
Neil Ellis <neilellis@cazcade.com>
2015-01-27 13:59:45 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
+module.exports= function(instanceConfig, projectConfig) {
 
-http.listen(8080, function(){
-  console.log('listening on 8080');
-});
+    io.on('connection', function(socket){
+      socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+      });
+    });
+    
+    http.listen(8080, function(){
+      console.log('listening on 8080');
+    });
+
+    return {server:http};
+};
