f253b56991203ec9997453c48f9891922af313d6
Neil Ellis <neilellis@cazcade.com>
2015-01-29 01:35:44 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var io = require('socket.io')(http, {'transports': ['websocket', 'polling']});
 
 module.exports= function(instanceConfig, projectConfig) {
 
